c107ef3974c048ddba40dd827bcfc449f29d2c38
Amed Reyes <areyes@nerd.com>
2017-05-23 11:26:57 -0400

    Initial version of working Datablocks transformer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict';
 const Transform = require('stream').Transform
+const log = require('util').debuglog('edf')
 
 
 const ENCODING      = 'ascii'
@@ -8,7 +9,55 @@ const HEADER_STATUS = 0
 const SIGNAL_STATUS = 1
 const DATA_STATUS   = 2
 
-function TransformState = function(){
+const signal_header_fields = [
+    {
+        name: "Label",
+        length: 16
+    },
+    {
+        name: "Transducer",
+        length: 80
+    },
+    {
+        name: "PhysicalDimensions",
+        length: 8
+    },
+    {
+        name: "PhysicalMin",
+        length: 8,
+        cast: parseFloat
+    },
+    {
+        name: "PhysicalMax",
+        length: 8,
+        cast: parseFloat
+    },
+    {
+        name: "DigitalMin",
+        length: 8,
+        cast: parseInt
+    },
+    {
+        name: "DigitalMax",
+        length: 8,
+        cast: parseInt
+    },
+    {
+        name: "Prefiltering",
+        length: 80
+    },
+    {
+        name: "Samples",
+        length: 8,
+        cast: parseInt
+    },
+    {
+        name: "Reserved",
+        length: 32
+    },
+]
+
+function TransformState() {
   this.stage = HEADER_STATUS
   this.buffer =  Buffer.allocUnsafe(HEADER_LENGTH),
   this.waterMark = 0
@@ -21,6 +70,8 @@ function TransformState = function(){
 
 var DataBlocks = class extends Transform {
   constructor (options) {
+
+    options = options || {}  
     options.decodeStrings = true
     options.readableObjectMode = false
     options.writableObjectMode = false
@@ -31,39 +82,204 @@ var DataBlocks = class extends Transform {
 
   _transform (chunk, encoding, callback) {
 
+    const input = chunk
+
     if (HEADER_STATUS == this.state.stage){
-      const used = read_header(status,chunk);
-      if(used && !status.error){
+      const used = read_header(this.state,chunk);
+      if(used && !this.state.error){
         //we are assuming header.Signals is always > 0
-        status.stage = SIGNAL_STATUS
+        this.state.stage = SIGNAL_STATUS
         chunk = chunk.slice(used)
-        console.log(status.header)
+        log(this.state.header)
       }
     }
 
     if(SIGNAL_STATUS == this.state.stage){
-        const used = read_signal_headers(status,chunk)
-        if(used && !status.error){
-            status.stage = DATA_STATUS
+        const used = read_signal_headers(this.state,chunk)
+        if(used && !this.state.error){
+            this.state.stage = DATA_STATUS
             chunk = chunk.slice(used)
-            console.log(status.signals)
+            log(this.state.signals)
         }
     }
 
     if(DATA_STATUS == this.state.stage){
-        let counter = 0
+
         while(chunk.length > 0){
-            const {used,parsed} = read_data_block(status,chunk);
+            const {used,parsed} = read_data_block(this.state,chunk);
             chunk = chunk.slice(used)
             if(parsed){
-                counter++
-                log(`block parsed: ${counter}`)
+                
+                this.push(this.state.block)
+                
             }
         }
     }
+
+    callback(this.state.error,input)
   }
+
+  
+}
+
+function read_header(status, data){
+
+    const to_copy = HEADER_LENGTH - status.waterMark
+
+    const copied = data.copy(
+        status.buffer,status.waterMark,0,Math.min(to_copy,data.length)
+    )
+
+    status.waterMark += copied
+    if(HEADER_LENGTH != status.waterMark){
+        return false
+    }
+
+    var header = {}
+    const result = parse_edf_header(status.buffer,header)
+    if (true != result){
+        status.error = result
+        return false
+    }
+
+    status.header = header
+    status.waterMark = 0
+
+    return copied
+}
+
+function parse_edf_header(buffer,header){
+    header.Version = parseInt(buffer.toString(ENCODING,0,8).trimRight())
+    header.Patient = buffer.toString(ENCODING,8,88).trimRight()
+    header.Id      = buffer.toString(ENCODING,88,168).trimRight()
+    //todo: convert to date time
+    header.Start   = buffer.toString(ENCODING,168,184).trimRight()
+    header.HeaderLength = parseInt(buffer.toString(ENCODING,184,192).trimRight())
+    header.Reserved = buffer.toString(ENCODING,192,236).trimRight()
+    header.DataRecords = parseInt(buffer.toString(ENCODING,236,244).trimRight())
+    //convert to duration
+    header.Duration = buffer.toString(ENCODING,244,252).trimRight()
+    header.Signals = parseInt(buffer.toString(ENCODING,252).trimRight())
+    return true;
+}
+
+function read_signal_headers(status,data){
+
+    //initialize
+    if (!status.signals){
+
+        //todo: may be find a more functional approarch to
+        //initialize the array
+        status.signals = []
+        for (let i = 0; i < status.header.Signals; i++) {
+            status.signals.push({})
+        }
+
+        //signal marker
+        status.buffer[HEADER_LENGTH - 1] = 0
+        //field marker
+        status.buffer[HEADER_LENGTH - 2] = 0
+    }
+
+    var current_field  = status.buffer[HEADER_LENGTH - 2]
+    var current_signal = status.buffer[HEADER_LENGTH - 1]
+    var waterMark      = status.waterMark
+    var used           = 0
+
+    for (let i = current_field; i < signal_header_fields.length; i++){
+        const field   = signal_header_fields[i]
+        const signals = status.signals.slice(current_signal)
+
+        const result = parse_signal_header_field(
+            field,signals,data.slice(used), status.buffer,waterMark
+        )
+
+        used +=field.length * (result.parsed) + result.waterMark
+
+        if (
+            result.waterMark != 0 ||
+            status.header.Signals != (current_signal + result.parsed)
+        ){
+            status.waterMark = result.waterMark
+            status.buffer[HEADER_LENGTH - 1] = current_signal + result.parsed
+            status.buffer[HEADER_LENGTH - 2] = current_field + i;
+            return false
+
+        }
+
+        current_signal = 0
+    }
+
+    return used
+
+}
+
+function parse_signal_header_field(field,signals,data,scratch_pad,waterMark){
+
+    const {name,length:FIELD_SIZE,cast=null} = field
+    const SIGNAL_TOTAL = signals.length
+    var buffer = scratch_pad.slice(0,FIELD_SIZE)
+    var i = 0
+    var read = 0
+
+    while(i < SIGNAL_TOTAL){
+        let to_copy = FIELD_SIZE - waterMark
+
+        let copied = data.copy(
+            buffer,buffer.waterMark,read, Math.min(read + to_copy,data.length)
+        )
+
+        waterMark += copied
+        read      += copied
+
+        if(FIELD_SIZE != waterMark){
+            break
+        }
+
+        let value = buffer.toString(ENCODING).trimRight()
+        if(cast){
+            value = cast(value)
+        }
+
+        signals[i][name] = value
+        i++
+        waterMark = 0
+    }
+
+    return {
+        waterMark : waterMark,
+        parsed    : i
+    }
+}
+
+function read_data_block(status, data){
+    //initialize
+    if(!status.block){
+        const block_size = status.signals.reduce((acc, signal) => {
+            return acc + signal.Samples*2
+        },0)
+        log(block_size)
+        status.block = Buffer.allocUnsafe(block_size)
+    }
+
+    var waterMark = status.waterMark
+    const to_copy = status.block.length - waterMark
+    const copied  = data.copy(
+        status.block, waterMark,0,Math.min(to_copy,data.length)
+    )
+
+    waterMark += copied
+    status.waterMark = (waterMark == status.block.length)? 0 : waterMark
+    if(!status.waterMark){
+      log("Block Parsed")
+    }
+
+    return {
+        used    : copied,
+        parsed  : (status.waterMark == 0)
+    }
 }
 
 module.exports = {
-  DataRecordTransform
+  DataBlocks
 }
